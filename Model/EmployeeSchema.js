5356c4a56da70cb0c828f03c5ae5934a9e45a50d
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2023-07-18 18:15:11 +0530

    schema changes for testing

--- Model/EmployeeSchema.js
+++ Model/EmployeeSchema.js
@@ -1,34 +1,31 @@
 const { default: mongoose } = require("mongoose");
 
 const employeeSchema = mongoose.Schema({
-  username : {
-    type : String,
-    required : [true, "give a username"]
+  username: {
+    type: String,
+    // required : [true, "give a username"]
   },
 
-  password : {
-    type : String,
-    required : [true, "give a password"]
-  },
-  
-  admin : {
-    type : Boolean,
-    required : [true, "give admin rights"]
-  },
-  attendance: {
-    type: [
-      {
-        date: {
-          type: Date,
-          _id: false,
-        },
-      },
-    ],
-    required: false,
+  password: {
+    type: String,
+    // required : [true, "give a password"]
   },
 
-
-
+  admin: {
+    type: Boolean,
+    // required : [true, "give admin rights"]
+  },
+  // attendance: {
+  //   type: [
+  //     {
+  //       date: {
+  //         type: Date,
+  //         _id: false,
+  //       },
+  //     },
+  //   ],
+  //   required: false,
+  // },
 });
 
 module.exports = mongoose.model("employee", employeeSchema);
